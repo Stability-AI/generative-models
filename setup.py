dec960139057e65a6933c99460c1a4ead5179dba
KARTHIK KUMAR GUPTA <137091753+KARTHIK07GUPTA@users.noreply.github.com>
2023-06-24 22:46:02 +0530

    Update setup.py
    author: Specifies the name of the package author.
    author_email: Specifies the email address of the package author.
    install_requires: Specifies the dependencies required by the package. Add the necessary dependencies as a list of strings.
    entry_points: Allows you to define command-line scripts or other entry points for your package. It associates the sgm-cli command with the main() function in the sgm.cli module.
    package_data: Specifies additional files or data to be included with the package. In this example, it includes all text files in the data directory of the sgm package.
    license: Specifies the license under which the package is distributed (e.g., "MIT", "Apache License 2.0", etc.).
    classifiers: Provides classifiers or tags for the package that indicate its properties, target audience, and compatibility.
    Feel free to modify these fields according to your package's specific requirements.

--- setup.py
+++ setup.py
@@ -3,11 +3,33 @@ from setuptools import find_packages, setup
 setup(
     name="sgm",
     version="0.0.1",
+    author="Your Name",
+    author_email="your_email@example.com",
     packages=find_packages(),
     python_requires=">=3.8",
-    py_modules=["sgm"],
+    install_requires=[
+        "dependency1",
+        "dependency2",
+    ],
+    entry_points={
+        "console_scripts": [
+            "sgm-cli=sgm.cli:main",
+        ],
+    },
+    package_data={
+        "sgm": ["data/*.txt"],
+    },
     description="Stability Generative Models",
     long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/Stability-AI/generative-models",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
 )
