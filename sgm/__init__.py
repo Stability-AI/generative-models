31f9662aaf0c8fb38dd12cdf6776da659d27cf90
Aarni Koskela <akx@iki.fi>
2023-06-27 11:23:37 +0300

    Use pyproject.toml + Hatch instead of setup.py
    Sibling of https://github.com/Stability-AI/stablediffusion/pull/269

--- sgm/__init__.py
+++ sgm/__init__.py
@@ -1,3 +1,5 @@
 from .data import StableDataModuleFromConfig
 from .models import AutoencodingEngine, DiffusionEngine
-from .util import instantiate_from_config
+from .util import instantiate_from_config, get_configs_path
+
+__version__ = "0.0.1"
--- sgm/util.py
+++ sgm/util.py
@@ -229,3 +229,21 @@ def load_model_from_config(config, ckpt, verbose=True, freeze=True):
 
     model.eval()
     return model
+
+
+def get_configs_path() -> str:
+    """
+    Get the `configs` directory.
+    For a working copy, this is the one in the root of the repository,
+    but for an installed copy, it's in the `sgm` package (see pyproject.toml).
+    """
+    this_dir = os.path.dirname(__file__)
+    candidates = (
+        os.path.join(this_dir, "configs"),
+        os.path.join(this_dir, "..", "configs"),
+    )
+    for candidate in candidates:
+        candidate = os.path.abspath(candidate)
+        if os.path.isdir(candidate):
+            return candidate
+    raise FileNotFoundError(f"Could not find SGM configs in {candidates}")
